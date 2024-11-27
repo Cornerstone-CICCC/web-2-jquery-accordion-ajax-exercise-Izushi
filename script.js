0fca7fb730491d10949ebd739bbf574d01c88f12
Daz <shoeshinesamurai@gmail.com>
2024-11-27 12:01:45 -0800

    accordion ajax exercise

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(function() {
   // your code here
+
+  $('.accordion-header').on('click', function() {
+    const content = $(this).next('.accordion-content');
+    $('.accordion-content').not(content).slideUp(1000);
+    content.slideToggle(1000);
+  });
+
+  $('.todos button').on('click', function() {
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function(res) {
+        const todos = res.todos;
+        todos.forEach(todo => {
+          $('.todos ul').append(`
+            <li>${todo.todo}</li>
+          `)
+        })
+      },
+      error: function(err) {
+        console.error(err);
+      }
+    })
+  });
 })
